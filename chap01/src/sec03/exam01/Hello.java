4cb967eaff79129acc2b49f316a9678a14d7b119
junyoungc <doforme524@gmail.com>
2021-07-27 00:07:32 +0900

    chap1-3 end

--- chap01/src/sec03/exam01/Hello.java
+++ chap01/src/sec03/exam01/Hello.java
@@ -1,10 +1,15 @@
 package sec03.exam01;
 
+/*
+ 작성자 : 최준영
+ 작성일 : 2021.7.27
+ */
 public class Hello {
-
+	//프로그램 실행 진입점
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// 콘솔에 출력하는 실행문
 		System.out.println("Hello, Java");
-	}
+	}// end of main
 
-}
+}// end of class
